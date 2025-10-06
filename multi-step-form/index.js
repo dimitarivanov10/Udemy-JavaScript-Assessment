15d8c762c027096f8a4ec7f56ff5463d6480a7e9
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-06 10:13:32 +0300

    Fixing the transition between pages

--- multi-step-form/index.js
+++ multi-step-form/index.js
@@ -1,13 +1,66 @@
-const inputFirstName = document.getElementById("first_name");
-const errorText = document.getElementById("first_name_error");
+let inputEl = null;
+let errorMessage = null;
 
-inputFirstName.addEventListener("blur", () => {
-  const regex = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
-  const value = inputFirstName.value.trim();
+const firstFieldSetter = document.getElementById("StepOneContainer");
+const secondFieldSetter = document.getElementById("StepTwoContainer");
+const oneToTwoBtn = document.getElementById("StepOneNext");
 
-  if (regex.test(value)) {
-    errorText.style.display = "none";
-  } else {
-    errorText.style.display = "block";
+document.addEventListener("click", (event) => {
+  const clickedElement = event.target;
+  const elementId = clickedElement.id;
+
+  switch (elementId) {
+    case "first_name":
+      inputEl = document.getElementById(elementId);
+      errorMessage = document.getElementById(`${elementId}_error`);
+
+      inputEl.addEventListener("blur", () => {
+        const regex = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
+        const value = inputEl.value.trim();
+
+        if (regex.test(value)) {
+          errorMessage.style.display = "none";
+        } else {
+          errorMessage.style.display = "block";
+        }
+      });
+      break;
+    case "last_name":
+      inputEl = document.getElementById(elementId);
+      errorMessage = document.getElementById(`${elementId}_error`);
+
+      inputEl.addEventListener("blur", () => {
+        const regex = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
+        const value = inputEl.value.trim();
+
+        if (regex.test(value)) {
+          errorMessage.style.display = "none";
+        } else {
+          errorMessage.style.display = "block";
+        }
+      });
+      break;
+
+    case "email":
+      inputEl = document.getElementById(elementId);
+      errorMessage = document.getElementById(`${elementId}_error`);
+
+      inputEl.addEventListener("blur", () => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const value = inputEl.value.trim();
+
+        if (regex.test(value)) {
+          errorMessage.style.display = "none";
+        } else {
+          errorMessage.style.display = "block";
+        }
+      });
+      break;
   }
 });
+
+oneToTwoBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    firstFieldSetter.style.display = "none";
+    secondFieldSetter.style.display = "block";
+})
