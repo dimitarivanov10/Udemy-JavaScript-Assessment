b9016ea6566f01b3d288ba9fa268e7473780ef06
Dimitar Ivanov <dimitucha2004@gmail.com>
2025-10-06 09:18:09 +0300

    Adding regex to check for real names

--- multi-step-form/index.js
+++ multi-step-form/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const inputFirstName = document.getElementById("first_name");
+const errorText = document.getElementById("first_name_error");
+
+inputFirstName.addEventListener("blur", () => {
+  const regex = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
+  const value = inputFirstName.value.trim();
+
+  if (regex.test(value)) {
+    errorText.style.display = "none";
+  } else {
+    errorText.style.display = "block";
+  }
+});
